1eab3926decd100bc5c40a95d74b0a69085c6441
Maulana Ridhwan <136357153+maulanaridhwan@users.noreply.github.com>
2024-07-18 10:02:51 +0700

    pred_app.py

--- pred_app.py
+++ pred_app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPTJeq6gLGkNxNQayoDl49f",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
